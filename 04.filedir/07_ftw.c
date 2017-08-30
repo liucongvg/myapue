52c673c1946f9a2cf1672a81d77aed15d220a85d
刘聪 <liucong@meizu.com>
2017-08-30 15:57:58 +0800

    routine commit

--- 04.filedir/07_ftw.c
+++ 04.filedir/07_ftw.c
@@ -25,19 +25,19 @@ int main(int argc, char* argv[])
     ntot = nreg + ndir + nblk + nfifo + nslink + nsock;
     if (ntot == 0)
         ntot = 1;
-    printf("regular files = %7ld, %5.2f %%\n", nreg, nreg * 100 / ntot);
-    printf("directories = %7ld, %5.2f %%\n", ndir, ndir * 100 / ntot);
-    printf("block special = %7ld, %5.2f %%\n", nblk, nblk * 100 / ntot);
-    printf("char special = %7ld, %5.2f %%\n", nchr, nchr * 100 / ntot);
-    printf("FIFOs special = %7ld, %5.2f %%\n", nfifo, nfifo * 100 / ntot);
-    printf("symbolic links = %7ld, %5.2f %%\n", nslink, nslink * 100 / ntot);
-    printf("sockets = %7ld, %5.2f %%\n", nsock, nsock * 100 / ntot);
+    printf("regular files = %7ld, %5.2f %%\n", nreg, nreg * 100.0 / ntot);
+    printf("directories = %7ld, %5.2f %%\n", ndir, ndir * 100.0 / ntot);
+    printf("block special = %7ld, %5.2f %%\n", nblk, nblk * 100.0 / ntot);
+    printf("char special = %7ld, %5.2f %%\n", nchr, nchr * 100.0 / ntot);
+    printf("FIFOs special = %7ld, %5.2f %%\n", nfifo, nfifo * 100.0 / ntot);
+    printf("symbolic links = %7ld, %5.2f %%\n", nslink, nslink * 100.0 / ntot);
+    printf("sockets = %7ld, %5.2f %%\n", nsock, nsock * 100.0 / ntot);
     return ret;
 }
 
 static char* fullpath;
 static size_t pathlen;
-int myftw(char* pathname, Myfunc* myfunc)
+static int myftw(char* pathname, Myfunc* myfunc)
 {
     fullpath = path_alloc(&pathlen);
     if (pathlen <= strlen(pathname)) {
@@ -56,7 +56,7 @@ int myftw(char* pathname, Myfunc* myfunc)
 #define FTW_DIRECTORY_READ_ERROR 3
 #define FTW_STAT_ERROR 4
 
-int dopath(Myfunc* myfunc)
+static int dopath(Myfunc* myfunc)
 {
     struct stat statbuf;
     struct dirent* dirp;
@@ -80,7 +80,7 @@ int dopath(Myfunc* myfunc)
     fullpath[n++] = '/';
     fullpath[n] = 0;
     if ((dp = opendir(fullpath)) == NULL)
-        return myfunc(fullpath, statbuf, FTW_DIRECTORY_READ_ERROR);
+        return myfunc(fullpath, &statbuf, FTW_DIRECTORY_READ_ERROR);
     while ((dirp = readdir(dp)) != NULL) {
         if (strcmp(dirp->d_name, ".") == 0 || strcmp(dirp->d_name, "..") == 0)
             continue;
@@ -90,7 +90,7 @@ int dopath(Myfunc* myfunc)
     }
     fullpath[n - 1] = 0;
     if (closedir(dp) < 0) {
-        perror("closedir error for %s", fullpath);
+        perror("closedir error");
         return -1;
     }
     return 0;
@@ -137,4 +137,47 @@ static int myfunc(const char* pathname, const struct stat* statptr, int type)
         break;
     }
     return 0;
+}
+
+#ifdef PATH_MAX
+static long pathmax = PATH_MAX;
+#else
+static long pathmax = 0;
+#endif
+
+static long posix_version = 0;
+static long xsi_version = 0;
+#define PATH_MAX_GUESS 1024
+
+static char* path_alloc(size_t* sizep)
+{
+    char* ptr;
+    size_t size;
+    if (posix_version == 0)
+        posix_version = sysconf(_SC_VERSION);
+    if (xsi_version == 0)
+        xsi_version = sysconf(_SC_XOPEN_VERSION);
+    if (pathmax == 0) {
+        errno = 0;
+        if ((pathmax = pathconf("/", _PC_PATH_MAX)) < 0) {
+            if (errno == 0)
+                pathmax = PATH_MAX_GUESS;
+            else {
+                perror("pathconf error");
+                exit(EXIT_FAILURE);
+            }
+        } else
+            ++pathmax;
+    }
+    if ((posix_version < 200112L) && (xsi_version < 4))
+        size = pathmax + 1;
+    else
+        size = pathmax;
+    if ((ptr = malloc(size)) == NULL) {
+        perror("malloc error");
+        exit(EXIT_FAILURE);
+    }
+    if (sizep != NULL)
+        *sizep = size;
+    return ptr;
 }
