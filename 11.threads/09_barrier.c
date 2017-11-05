228445145e26ef577e19b31b1249de0d2b3a8775
刘聪 <liucong@meizu.com>
2017-11-05 23:30:35 +0800

    routine commit

--- 11.threads/09_barrier.c
+++ 11.threads/09_barrier.c
@@ -50,8 +50,45 @@ void node_adjust_down(int array[], int index, int length)
     }
 }
 
+#define THREAD_COUNT 8
+#define NUMBER_COUNT 80000000
+#define THREAD_NUMBER_COUNT (NUMBER_COUNT / THREAD_COUNT)
+int random_numbers[NUMBER_COUNT];
+int sorted_numbers[NUMBER_COUNT];
+pthread_barrier_t barrier;
+
+void* thread_function(void* arg)
+{
+    int thread_index = (int)arg;
+    heap_sort(&random_numbers[thread_index * THREAD_NUMBER_COUNT], THREAD_NUMBER_COUNT);
+    pthread_barrier_wait(&barrier);
+}
+
+void merge()
+{
+    int i, j;
+    int thread_initial_index[THREAD_COUNT];
+    for (i = 0; i < THREAD_COUNT; ++i) {
+        thread_initial_index[i] = THREAD_NUMBER_COUNT * i;
+    }
+    for (i = 0; i < NUMBER_COUNT; ++i) {
+        int min_thread_index = INT_MAX;
+        int min_number = RAND_MAX;
+        for (j = 0; j < THREAD_COUNT; ++j) {
+            if (random_numbers[thread_initial_index[j]] < min_number
+                && thread_initial_index[j] < (j + 1) * THREAD_NUMBER_COUNT) {
+                min_thread_index = j;
+                min_number = random_numbers[thread_initial_index[j]];
+            }
+        }
+        ++thread_initial_index[min_thread_index];
+        sorted_numbers[i] = min_number;
+    }
+}
+
 int main(int argc, char* argv[])
 {
+    /*
     int i;
     int array[MAX_COUNT];
     srand(1);
@@ -61,5 +98,27 @@ int main(int argc, char* argv[])
     for (i = 0; i < MAX_COUNT; ++i)
         printf("%d,", array[i]);
     printf("\n");
+    */
+    int i;
+    for (i = 0; i < NUMBER_COUNT; ++i) {
+        random_numbers[i] = random() % NUMBER_COUNT;
+    }
+    pthread_t tid;
+    pthread_barrier_init(&barrier, NULL, THREAD_COUNT + 1);
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    for (i = 0; i < THREAD_COUNT; ++i) {
+        if (pthread_create(&tid, NULL, thread_function, (void*)i))
+            printf("error create thread\n");
+    }
+    pthread_barrier_wait(&barrier);
+    merge();
+    gettimeofday(&end, NULL);
+    long time_took = end.tv_sec - start.tv_sec;
+    /*
+    for (i = 0; i < NUMBER_COUNT; ++i)
+        printf("%d\n", sorted_numbers[i]);
+    */
+    printf("sort time took:%ld\n", time_took);
     return 0;
 }
