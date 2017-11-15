3d6469fecab6332bd1817d36249b65fd0c1d44f1
刘聪 <liucong@meizu.com>
2017-11-15 23:57:05 +0800

    routine commit

---
+++ 14.advio/02_deadlock.c
@@ -0,0 +1,89 @@
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+static void lock_a_byte(const char* name, int fd, int offset);
+static void tell_wait();
+static void wait_parent();
+static void wait_child();
+static void tell_parent();
+static void tell_child();
+
+int main(int argc, char* argv[])
+{
+    int fd = creat("tempfile", S_IRWXU | S_IRWXG | S_IRWXO);
+    if (fd < 0)
+        return -1;
+    write(fd, "aa", 2);
+    tell_wait();
+    pid_t pid;
+    if ((pid = fork()) < 0)
+        return -1;
+    else if (pid == 0) {
+        lock_a_byte("child", fd, 0);
+        tell_parent(getppid());
+        wait_parent();
+        lock_a_byte("child", fd, 1);
+    } else {
+        lock_a_byte("parent", fd, 1);
+        tell_child(pid);
+        wait_child();
+        lock_a_byte("parent", fd, 0);
+    }
+    return 0;
+}
+
+static void lock_a_byte(const char* name, int fd, int offset)
+{
+    struct flock my_lock;
+    my_lock.l_len = 1;
+    my_lock.l_whence = SEEK_SET;
+    my_lock.l_start = offset;
+    my_lock.l_type = F_WRLCK;
+    int res = fcntl(fd, F_SETLK, &my_lock);
+    if (res < 0) {
+        fprintf(stderr, "%s lock:%d ", name, offset);
+        perror("fcntl error");
+    } else
+        printf("%s hold lock, offset:%d\n", name, offset);
+}
+
+void sig_usr1(int sig_nu) { printf("sig_usr1\n"); }
+struct sigaction action;
+static void tell_wait()
+{
+    action.sa_handler = sig_usr1;
+    sigemptyset(&action.sa_mask);
+    sigaction(SIGUSR1, &action, NULL);
+}
+static void wait()
+{
+    sigset_t set;
+    sigemptyset(&set);
+    sigsuspend(&set);
+}
+static void wait_parent()
+{
+    printf("wait parent\n");
+    wait();
+}
+static void wait_child()
+{
+    printf("wait child\n");
+    wait();
+}
+static void tell(pid_t pid) { kill(pid, SIGUSR1); }
+static void tell_parent(pid_t pid)
+{
+    printf("tell parent\n");
+    tell(pid);
+}
+static void tell_child(pid_t pid)
+{
+    printf("tell child\n");
+    tell(pid);
+}
