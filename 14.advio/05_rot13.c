7734c720387fef2cb538d50dd9b220b2e1171b7a
刘聪 <liucong@meizu.com>
2017-11-23 23:32:35 +0800

    routine commit

--- 14.advio/05_rot13.c
+++ 14.advio/05_rot13.c
@@ -1,6 +1,9 @@
 #include <aio.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -26,6 +29,39 @@ int main(int argc, char* argv[])
         return -1;
     struct stat statbuf;
     fstat(in_fd, &statbuf);
-    struct aio_wrap wraps[BSZ];
-
+    struct aio_wrap wraps[NBUF];
+    memset(wraps, 0, sizeof(wraps));
+    int i;
+    const struct aiocb* aio_list[NBUF];
+    for (i = 0; i < BSZ; ++i) {
+        wraps[i].op = UNUSED;
+        wraps[i].aiocb.aio_buf = wraps[i].data;
+        wraps[i].aiocb.aio_sigevent.sigev_notify = SIGEV_NONE;
+        aio_list[i] = NULL;
+    }
+    int offset = 0;
+    int res;
+    int num_to_process = 0;
+    while (1) {
+        for (i = 0; i < NBUF; ++i) {
+            switch (wraps[i].op) {
+            case UNUSED:
+                if (offset >= statbuf.st_size)
+                    continue;
+                wraps[i].op = READ_PENDING;
+                wraps[i].aiocb.aio_fildes = in_fd;
+                wraps[i].aiocb.aio_offset = offset;
+                offset += BSZ;
+                if (offset >= statbuf.st_size)
+                    wraps[i].last = 1;
+                wraps[i].aiocb.aio_nbytes = BSZ;
+                if (aio_read(&wraps[i].aiocb) < 0) {
+                    printf("error in aio_read\n");
+                    exit(-1);
+                }
+                aio_list[i] = &wraps[i].aiocb;
+                ++num_to_process;
+            }
+        }
+    }
 }
